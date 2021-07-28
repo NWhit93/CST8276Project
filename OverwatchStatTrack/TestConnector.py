dde24bf91af2579d126d4e1d70d7d4bafdf8ef99
NATHEN-PC\nathe <whit1023@algonquinlive.com>
2021-07-28 16:17:04 -0400

    First commit

---
+++ OverwatchStatTrack/TestConnector.py
@@ -0,0 +1,34 @@
+import pyrebase
+
+config = {
+    "apiKey" : "AIzaSyDg_XVDSQJ5q3_V3F5pWvqLNHlDKwZXDZY",
+    "authDomain" : "cst8276project-af898.firebaseapp.com",
+    "databaseURL" : "https://cst8276project-af898-default-rtdb.firebaseio.com/",
+    "projectId" : "cst8276project-af898",
+    "storageBucket" : "cst8276project-af898.appspot.com",
+    "messagingSenderId" : "532154756835",
+    "appId" : "1:532154756835:web:ab7d4eceb3554c14795fd6"
+    }
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+# data = {'Name' : 'Monkeh'}
+# db.child('Heroes').push(data)
+
+heroes = db.child("Heroes").get()
+for hero in heroes:
+    print(hero.key())
+    print(hero.val())
+
+heroes = db.child("Heroes").shallow().get()
+print(heroes.val())
+print(type(heroes))
+for a in heroes:
+    h = db.child(a).get()
+    print(h.val())
+
+# print("Database returned:")
+# print(printList)
+# print("Done")
